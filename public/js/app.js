8cfa53fcb14e4fa6da47284f186b5aee15515bc2
Luke Stern <luke.stern@softwire.com>
2021-06-10 16:26:13 +0100

    Mobile menu & random background colours

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     const randomButton = document.getElementById("random-colour-button");
-    
-    randomButton.addEventListener('click', _ => {
-        const posts = getAllPosts();
-        for (let element of posts) {
-            changeBackgroundColour(element);
-        };
-    } )
+    if (randomButton){
+        randomButton.addEventListener('click', _ => {
+            const posts = getAllPosts();
+            for (let element of posts) {
+                changeBackgroundColour(element);
+            };
+        })
+    }
 
     function getAllPosts() {
-        return document.getElementsByClassName("post-listing")
+        return document.getElementsByClassName("post-listing");
     }
 
     function changeBackgroundColour(element) {
-        element.style.background = `rgb(${Math.random() * 100}, ${Math.random() * 100}, ${Math.random() * 100})`
+        element.style.background = `rgb(${Math.random() * 100}, ${Math.random() * 100}, ${Math.random() * 100})`;
     }
+
+    const menuButton = document.getElementById("menu-button");
+
+    menuButton.addEventListener('click', _ => {
+        document.getElementById("menu-container").style.display = "flex";
+        document.getElementById("page-container").style.filter = "blur(5px)";
+        document.getElementsByClassName("page-container")[0].style.filter = "blur(5px)";
+    })
+
+    const closeButton = document.getElementById("close-button");
+
+    closeButton.addEventListener('click', _ => {
+        document.getElementById("menu-container").style.display = "none";
+        document.getElementById("page-container").style.filter = "blur(0)";
+        document.getElementsByClassName("page-container")[0].style.filter = "blur(0)";
+    })
 });
