721eebde837c851977f7134200626b462d27f915
Luke Stern <luke.stern@softwire.com>
2021-06-10 11:15:35 +0100

    random background colour for posts

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
+    const randomButton = document.getElementById("random-colour-button");
+    
+    randomButton.addEventListener('click', _ => {
+        const posts = getAllPosts();
+        for (let element of posts) {
+            changeBackgroundColour(element);
+        };
+    } )
+
+    function getAllPosts() {
+        return document.getElementsByClassName("post-listing")
+    }
+
+    function changeBackgroundColour(element) {
+        element.style.background = `rgb(${Math.random() * 100}, ${Math.random() * 100}, ${Math.random() * 100})`
+    }
 });
