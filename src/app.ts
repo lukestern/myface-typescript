c2b6e67f983e435a9aff8a050578e4124ede78a2
Luke Stern <luke.stern@softwire.com>
2021-06-15 15:48:02 +0100

    Initial components

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import postRoutes from "./routes/postRoutes";
 import userRoutes from "./routes/userRoutes";
 
 const app = express();
-const port = process.env['PORT'] || 3001;
+const port = process.env['PORT'] || 3002;
 
 app.use(express.static('public'));
 app.use(express.json());
